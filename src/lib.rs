db618e88ea021f8b6c58b46fd77790f78171bbdf
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-09-07 21:25:47 -0400

    Initial (Moved from pipeworkmc-server);

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![feature(
+
+    // Syntax
+    decl_macro,
+
+    // Standard library
+    maybe_uninit_array_assume_init
+
+)]
+
+
+pub mod decode;
+pub mod encode;
+pub mod meta;
+
+pub use uuid;
+pub mod varint;
---
+++ src/meta.rs
@@ -0,0 +1,25 @@
+use atomic_enum::atomic_enum;
+
+
+pub trait PacketMeta {
+    const STATE  : PacketState;
+    const BOUND  : PacketBound;
+    const PREFIX : u8;
+}
+
+#[atomic_enum]
+#[derive(PartialEq, Eq, Hash, Default)]
+pub enum PacketState {
+    #[default]
+    Handshake,
+    Status,
+    Login,
+    Config,
+    Play
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum PacketBound {
+    S2C,
+    C2S
+}
